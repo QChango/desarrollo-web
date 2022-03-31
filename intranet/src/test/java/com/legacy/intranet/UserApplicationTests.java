4fc15c781fb4df1f0903df16a6f49f18c77723f7
QChango <molero.marcos@hotmail.com>
2022-03-30 20:05:15 -0400

    Initial commit for Project #1

---
+++ intranet/src/test/java/com/legacy/intranet/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.legacy.intranet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
